a545704bdbcf7a6ac61ef2bca24dbb0645d15ce6
JBorrow <joshua.borrow@durham.ac.uk>
2014-09-15 17:14:59 +0100

    Added a real event

---
+++ Events/1.php
@@ -0,0 +1,21 @@
+<h1>Welcome Freshers!</h1>
+
+<img src="http://media.tumblr.com/63dfddc06cd359fc521ead32793af8a3/tumblr_inline_mu98bxIlGL1qiihii.jpg" alt="Good Luck!" />
+
+<h2> 15th September 2014 </h2>
+
+<p>We would like to extend a warm welcome to all of the freshers joining Durham
+in October. We are sure that you will have a great time during freshers' week
+and beyond! Good luck trying to stave off fresher's flu - according to the BBC
+<a href = "http://www.bbc.co.uk/dna/hub/A645211">more than 90% of new students
+get it</a>. I'd recommend stocking up on cold and flu remedies as soon as you
+can!</p>
+
+<p>If you're having any trouble choosing modules, or are unsure if you have made
+the correct choice and want to speak to someone in a similar position to you,
+don't forget that you can always e-mail us at any time on any matter at
+<a href='mailto&#58;n&#97;t&#46;sc%6&#57;&#46;soc%4&#48;du%72&#37;&#54;8am%2Ea&#37;6&#51;&#46;u%6B'>nat&#46;sci&#46;&#115;oc&#64;d&#117;rha&#109;&#46;&#97;c&#46;u&#107;</a>.</p>
+
+<p>Have a fantastic time - you'll miss it when lectures start. Good luck in your
+first year and we'll see you at the freshers' fair!</p>
+
--- Events/testevent.php
+++
@@ -1,5 +0,0 @@
-<h1>Test Event</h1>
-
-<img src="http://www.yourmomhatesthis.com/images/Resim-botu-test-konusu-20.jpg" alt="Test" />
-
-<p>Well then this is a test event great very good thanks</p>
