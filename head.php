b06245e6489bdd5f89f1bb1f4eb6377015983b82
JBorrow <joshua.borrow@durham.ac.uk>
2014-09-15 17:15:07 +0100

    Added title

--- head.php
+++ head.php
@@ -1,3 +1,4 @@
 <link rel='stylesheet' type='text/css' href='noramlize.css' />
 <link rel='stylesheet' type='text/css' href='stylesheet.css' />
 <link href='http://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet' type='text/css'>
+<title>Durham Natural Sciences Society</title>
