18dbee1ab46ee549fe412802ad2c0b0e4c8be3d8
JBorrow <joshua.borrow@durham.ac.uk>
2014-09-23 18:00:49 +0100

    Added page about student support

---
+++ support.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<?php
+		$thisPage = "Support";
+		require_once('head.php');
+		?>
+	</head>
+
+	<body>
+		<?php
+		require_once('title.php');
+		require_once('navbar.php');
+		?>
+
+		<div class="container">
+			<div class="main">
+				<h1>My department is being annoying! Help!</h1>
+				<p>If a department is giving you trouble, ignoring you or won't
+				let you do one of their modules without a good reason, contact
+				us. We're here to help you! An e-mail from us has a lot more
+				weighting than you fighting on your own. We are also able to put
+				you in contact with others who could be able to help if we are
+				unsucessful.</p>
+				<h1>Mentor Scheme for Science Students</h1>
+				<p>If you would like some advice about modules from someone who
+				has actually studied them, then we as a society are here for
+				you. Contact us at <a href='mai&#108;t&#111;&#58;na%&#55;4&#46;s%63i&#46;so%63&#64;d&#117;rham&#46;a%&#54;3&#46;u&#107;'>nat&#46;s&#99;i&#46;&#115;oc&#64;dur&#104;am&#46;ac&#46;uk</a>,
+				letting us know what modules you would like advice about. We
+				will then put you in contact with someone in a higher year who
+				has done that module. If you're wanting to give this support to
+				people in the lower years, please contact us!</p>
+			</div>
+			</div>
+
+			<div class="sidebar">
+				<?php
+				require_once('sidebar.php');
+				?>
+			</div>
+		</div>
+	
+		<?php
+		require_once('footer.php')
+		?>
+	</body>
+</html>
