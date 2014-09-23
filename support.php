ced6c2a9cc68dbc11e82fcf7ce791e992b6c5a78
JBorrow <joshua.borrow@durham.ac.uk>
2014-09-23 18:05:49 +0100

    ver1.0

--- support.php
+++ support.php
@@ -23,7 +23,8 @@
 				weighting than you fighting on your own. We are also able to put
 				you in contact with others who could be able to help if we are
 				unsucessful.</p>
-				<h1>Mentor Scheme for Science Students</h1>
+				
+				<h1>Mentor scheme for science students</h1>
 				<p>If you would like some advice about modules from someone who
 				has actually studied them, then we as a society are here for
 				you. Contact us at <a href='mai&#108;t&#111;&#58;na%&#55;4&#46;s%63i&#46;so%63&#64;d&#117;rham&#46;a%&#54;3&#46;u&#107;'>nat&#46;s&#99;i&#46;&#115;oc&#64;dur&#104;am&#46;ac&#46;uk</a>,
@@ -31,6 +32,14 @@
 				will then put you in contact with someone in a higher year who
 				has done that module. If you're wanting to give this support to
 				people in the lower years, please contact us!</p>
+				
+				<h1>Anything else!</h1>
+				<p>If you are thinking about applying to Durham to do Natural
+				Sciences and would like to chat to someone aout what the course
+				is like, then please feel free to contact us. If there's
+				anything else that you think we could help with (e.g. setting
+				up a new science based event) then drop us an e-mail or Facebook
+				message. We really are here to help!
 			</div>
 			</div>
 
