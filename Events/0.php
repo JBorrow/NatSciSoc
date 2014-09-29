4f91333c52fb049f154a8e411a2728ceace76db9
JBorrow <joshua.borrow@durham.ac.uk>
2014-09-29 13:28:19 +0100

    Added rubiks cube event

---
+++ Events/0.php
@@ -0,0 +1,30 @@
+<h1>Rubik's Cube Bar Crawl</h1>
+
+<img src = "http://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Rubik's_cube.svg/983px-Rubik's_cube.svg.png" alt = "A rubik's cube!" />
+
+<h2>11th October 2014</h2>
+
+<p>Welcome to our first event of the year! We're going to be running a hill
+college bar crawl with a Rubik's Cube theme!</p>
+
+<p>We're going to meet at Grey College Bar at 8:00 pm on the 11th October 2014.
+</p>
+
+<p>Come along for a relaxed evening of fun, to meet scientists from all years
+and visit different colleges!</p>
+
+<p>We would like you to wear item(s) of clothing that correspond to the
+colour(s) of the subject(s) that you study! Everyone is welcome!</p>
+
+<ul>
+	<li>Biology - Blue</li>
+	<li>Physics - Green</li>
+	<li>Chemistry - Purple</li>
+	<li>Psychology - Red</li>
+	<li>Maths - White</li>
+	<li>Computer Science - Orange</li>
+	<li>Earth Sciences - Yellow</li>
+	<li>Engineering - Pink</li>
+	<li>Anthropology and Archaeology - Grey</li>
+	<li>Geography, Philosophy, Business, Economics and other - Black</li>
+</ul>
