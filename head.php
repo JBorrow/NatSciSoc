f4b9555140e1ad16f1e877f8854ddc1da29a647e
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-10-15 18:38:54 +0100

    Added jobs

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <link rel='stylesheet' type='text/css' href='noramlize.css' />
 <link rel='stylesheet' type='text/css' href='stylesheet.css' />
 <link href='http://fonts.googleapis.com/css?family=Rokkitt' rel='stylesheet' type='text/css'>
+<meta charset="utf-8">
 <title>Durham Natural Sciences Society</title>
