c4419cd6cc3947500af057eaaf0d29b9cd7e12e4
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-10-30 17:25:41 +0000

    changed to new

--- jobs.php
+++ jobs.php
@@ -19,175 +19,45 @@
 			
 				<h1>Graduate Opportunities and Internships</h1>
 
-				<h1>Teach First</h1>
+				<h1>M&G Investments</h1>
 
 				<ul class="plain">
-					<li>Position: Leadership Development Scheme</li>
-					<li>Company: Teach First</li>
-					<li>Industry: Eductation</li>
-					<li>Location: All regions in England & Wales</li>
-					<li>How to Apply: <a href="http://bit.ly/1qKuZuf">http://bit.ly/1qKuZuf</a> quoting ‘DUR NATSCI’</li>
-					<li>Start Date: June 2015</li>
-					<li>Earnings: £17,500+ (£21k+ in London)</li>
-					<li>Application Deadline: Apply as soon as possible</li>
+					<li>Internship:
+					  <ul>
+					    <li>Summer Internship – Information Systems</li>
+					    <li>Summer Internship – Change Management (Business & Tech)</li>
+					    <li>1 year placement – Information Systems</li>
+					  </ul>
+					</li>
+					<li>Industry: Investment Management</li>
+					<li>Location: London</li>
+					<li>Salary: Excellent</li>
+					<li>How to Apply:
+					   <ul>
+					    <li>Summer Internship – Information Systems <a href="http://sanctuarygraduates.us8.list-manage.com/track/click?u=b501d62ac727902781647f6ff&id=664451d9af&e=796d4e8e98">Click Here</a></li>
+					    <li>Summer Internship – Change Management (Business & Tech) <a href = "http://sanctuarygraduates.us8.list-manage2.com/track/click?u=b501d62ac727902781647f6ff&id=7b8a11e803&e=796d4e8e98">Click Here</a></li>
+					    <li>1 year placement – Information Systems <a href="http://sanctuarygraduates.us8.list-manage.com/track/click?u=b501d62ac727902781647f6ff&id=38479eb276&e=796d4e8e98">Click Here</a></li>
+					  </ul>
+					<li>Start Date: Summer 2015</li>
+					<li>Application Deadline: 28 November</li>
 				</ul>
 
-				<h2>What is Teach First?</h2>
+				<h2>The Company</h2>
 
-				<p>It is a UK charity offering graduates the opportunity to teach, inspire and change the lives of students in challenging circumstances. Their goal is to create future leaders who can raise pupil’s achievement levels and help break the link between low family income and poor educational attainment. Teach First are sponsored by many Times Top 100 Graduate Employers, including Accenture, Deloitte, Goldman Sachs, HSBC, Credit Suisse and Google, who look to recruit graduates who have completed the Teach First scheme.</p>
+				<p>Our client is one of the largest investment houses in the UK and one of Europe’s leading asset managers with over £240 billion in funds under management. Their company culture is famous. They have a strong belief in their staff having a great work-life balance. They have a very friendly working atmosphere and high staff morale. This emphasis on the culture of the workplace makes this global asset manager one of the best places to work, with unusually low staff turnover. This company look at the internship as an 8 week interview for the graduate scheme. The graduate scheme offers incredible training, a high salary, professional qualifications including the IMC and diverse rotations across the business.</p>
 			
-				<h2>What is the Leadership Development Scheme?</h2>
-
-				<p>After intensive training, the prestigious 2-year scheme is spent teaching in a disadvantaged school and you take on proper responsibility from day one. You will train for a PGCE, become a ‘newly qualified teacher’ in your second year and enjoy a salary increase.</p>
-				<p>The scheme also lets you complete a summer internship with one of their sponsor companies. These internships frequently lead to graduate job offers at the end of the scheme as many employers look to hire Teach First graduates. Remember joining Teach First doesn’t commit you to become a teacher after 2 years. This scheme is about changing children’s lives whilst dramatically improving your own leadership and employability skills.</p>
-
-				<h2>Is this for you?</h2>
-				<p> Are you a STEM student?</p>
-				<p>Would you like to teach Maths, Science, Computer Science, ICT or Design?</p>
-				<p><b>If so, please apply here <a href="http://bit.ly/1qKuZuf">http://bit.ly/1qKuZuf</a></b></p>
-				<p>You <b>do not</b> need to have any teaching experience.</p>
-
-				<p>After applying, Teach First will be in touch to help you start your application, but you must apply to us first. This is a free service.</p>
-
-  <h1>Girls Wanted for Investment Management (M&G)</h1>
-  <p align="left" style="margin-bottom: 0cm">Are you interested in a
-career within investments? You might then be interested in being an
-Investment Management <strong>Graduate or Summer Intern</strong> with
-our client, a global asset management company based in London.</p>
-<table width="311" cellpadding="3" cellspacing="0">
-	<col width="105">
-	<col width="194">
-	<tr>
-		<td width="105" style="border: none; padding: 0cm">
-			<p align="left"><strong>Industry:</strong></p>
-		</td>
-		<td width="194" style="border: none; padding: 0cm">
-			<p align="left">Investment Management</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="105" style="border: none; padding: 0cm">
-			<p align="left"><strong>Position:</strong></p>
-		</td>
-		<td width="194" style="border: none; padding: 0cm">
-			<p align="left">1.Investment Graduate Scheme;<br>2.Investment
-			Internship</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="105" style="border: none; padding: 0cm">
-			<p align="left"><strong>Location:</strong></p>
-		</td>
-		<td width="194" style="border: none; padding: 0cm">
-			<p align="left">London</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="105" style="border: none; padding: 0cm">
-			<p align="left"><strong>Salary</strong></p>
-		</td>
-		<td width="194" style="border: none; padding: 0cm">
-			<p align="left">£Excellent; IMC/CFA qualifications supported</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="105" style="border: none; padding: 0cm">
-			<p align="left"><strong>Start date:</strong></p>
-		</td>
-		<td width="194" style="border: none; padding: 0cm">
-			<p align="left">1.Autumn 2015; 2.Summer 2015</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="105" style="border: none; padding: 0cm">
-			<p align="left"><strong>How to apply:</strong></p>
-		</td>
-		<td width="194" style="border: none; padding: 0cm">
-			<p align="left">1.Graduate Scheme: <strong><a href="http://sanctuarygraduates.us8.list-manage2.com/track/click?u=b501d62ac727902781647f6ff&amp;id=d7b7141444&amp;e=796d4e8e98">Click
-			Here</a></strong><br>2.Internship: <strong><a href="http://sanctuarygraduates.us8.list-manage1.com/track/click?u=b501d62ac727902781647f6ff&amp;id=54b3d5844f&amp;e=796d4e8e98">Click
-			Here</a></strong></p>
-		</td>
-	</tr>
-	<tr>
-		<td width="105" style="border: none; padding: 0cm">
-			<p align="left"><strong>Deadline:</strong></p>
-		</td>
-		<td width="194" style="border: none; padding: 0cm">
-			<p align="left">27<sup>th</sup> October 2014</p>
-		</td>
-	</tr>
-	<tr>
-		<td width="105" style="border: none; padding: 0cm">
-			<p align="left"><strong>Referral code:</strong></p>
-		</td>
-		<td width="194" style="border: none; padding: 0cm">
-			<p align="left">DUR NATSCI</p>
-		</td>
-	</tr>
-</table>
-<h2>The Company</h2>Our client is
-one of the largest investment houses in the UK and one of Europe’s
-leading asset managers with&nbsp;over £240 billion in funds under
-management. Their company culture is famous. They have a strong
-belief in their staff having a great work-life balance. They have a
-very friendly working atmosphere and high staff morale. This emphasis
-on the culture of the workplace makes this global asset manager one
-of the best places to work, with unusually low staff turnover.
-Graduates benefit from a six-week induction so you can learn about
-all aspects of the business and investments, you will have your IMC
-and CFA qualifications supported and funded, and you will have
-on-going personal development training and mentoring.
-
-<h2>The
-Scheme</h2><strong>Graduates:</strong> The investment
-graduate scheme typically lasts between 12 and 18 months and involves
-a series of investment placements in this company’s business units.
-Trainees will rotate through areas including Fixed Income and
-Equities.<br><strong>Interns:</strong>The scheme lasts a minimum of 8
-weeks and interns will be placed in one specific investment
-area.
-<h2>What are the selling points of the Investment
-scheme?</h2> 
-<ul class='plain'>
-	<li>Professional
-	qualification in Investment Management Certificate (IMC) supported
-	and funded 
-
-	<li>Once completed,
-	supported study available for Chartered Financial Analyst (CFA)
-	qualification 
-
-	<li>Rotations give exposure to all aspects of
-	investments within the company 
-
-</ul>
-<h2>Who would suit this Investment scheme?</h2>
-<ul class= "plain">
-	<li><em>Those interested
-	in a career in investments</em> 
-
-	<li><p align="left" style="margin-bottom: 0cm">Those with the
-	following: 
-	</p>
-	<ul>
-		<li>Very strong
-		academics (340 UCAS, 2.1) 
-
-		<li>Excellent
-		decision-making skills 
-
-		<li>Strong analytical,
-		problem-solving and communication skills. 
-
-		<li>Ability to plan and think long term 
-	</ul>
-</ul>
-<p align="left"><br><strong>How to apply:</strong><br>1. Graduate
-Scheme: <a href="http://sanctuarygraduates.us8.list-manage1.com/track/click?u=b501d62ac727902781647f6ff&amp;id=01ae2831d6&amp;e=796d4e8e98">Click
-Here</a><br>2. Internship: <a href="http://sanctuarygraduates.us8.list-manage.com/track/click?u=b501d62ac727902781647f6ff&amp;id=5d7e1d3fac&amp;e=796d4e8e98">Click
-Here</a></p>
-
-
+				<h2>The Internships</h2>
+				
+				<h3>Information Systems</h3>
+				<p>Information Systems is a support function within the company that delivers innovative solutions to solve their IT problems. The company’s focus on creating and testing their own software makes them unique in the finance industry and a very exciting place to work in for any person interested in IT.  The internship will be in one of the following areas: Architecture, Development, Testing & Release & Support.</p>
+				<p>This 8 week summer internship is for anyone studying any degree but who likes technology and has demonstrable interest in pursuing a career in technology. You don’t have to be technically proficient (but there are projects for those that are).</p>
+				
+				<h3>Change Management</h3>
+				<p>Change Management.  Business and Technology Change is the primary interface for all of the company’s business units, working closely with each to understand the requirements for change and ultimately deliver the defined objectives within a project life-cycle environment. Projects can range from improvements to existing business solutions, implementation of new business and IT solutions, through to multi-million pound revenue generating business initiatives.</p>
+				<p>This 8 week summer internship is for anyone studying any degree who is interested in business strategy, technology and stakeholder management. You don’t need to be technically proficient.</p>
+				
+				<h3>Year Long Placement - Information Systems</h3>
+				<p>Year Long Placement - Information Systems: As the Information Systems internship above, but 12 months in duration with rotations around Architecture, Development, Testing & Release & Support. </p>
 			</div>
 			<div class="sidebar">
 				<?php
