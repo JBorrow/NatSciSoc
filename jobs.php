19f70f38e5d073d29c4152459230ee913aad6c94
JBorrow <joshua.borrow@durham.ac.uk>
2014-11-20 13:40:10 +0000

    added teachfirst

--- jobs.php
+++ jobs.php
@@ -19,45 +19,26 @@
 			
 				<h1>Graduate Opportunities and Internships</h1>
 
-				<h1>M&G Investments</h1>
+				<h1>Teach First Leadership Development Scheme</h1>
 
 				<ul class="plain">
-					<li>Internship:
-					  <ul>
-					    <li>Summer Internship – Information Systems</li>
-					    <li>Summer Internship – Change Management (Business & Tech)</li>
-					    <li>1 year placement – Information Systems</li>
-					  </ul>
-					</li>
-					<li>Industry: Investment Management</li>
-					<li>Location: London</li>
-					<li>Salary: Excellent</li>
-					<li>How to Apply:
-					   <ul>
-					    <li>Summer Internship – Information Systems <a href="http://sanctuarygraduates.us8.list-manage.com/track/click?u=b501d62ac727902781647f6ff&id=664451d9af&e=796d4e8e98">Click Here</a></li>
-					    <li>Summer Internship – Change Management (Business & Tech) <a href = "http://sanctuarygraduates.us8.list-manage2.com/track/click?u=b501d62ac727902781647f6ff&id=7b8a11e803&e=796d4e8e98">Click Here</a></li>
-					    <li>1 year placement – Information Systems <a href="http://sanctuarygraduates.us8.list-manage.com/track/click?u=b501d62ac727902781647f6ff&id=38479eb276&e=796d4e8e98">Click Here</a></li>
-					  </ul>
-					<li>Start Date: Summer 2015</li>
-					<li>Application Deadline: 28 November</li>
+					<li>Company: Teach First</li>
+					<li>Industry: Education</li>
+					<li>Location: All regions in England & Wales</li>
+					<li>Start date: June 2015</li>
+					<li>Earnings: £17,500+ (£21k+ in London) +£4k after first year</li>
+					<li>Interested? <a href="http://bc.tbe.taleo.net/BC5/ats/careers/requisition.jsp?org=SANCGRAD&cws=37&rid=51">Click here</a> quoting 'DUR NATSCI' as the referral code</li>
 				</ul>
 
-				<h2>The Company</h2>
+				<h2>What is Teach First?</h2>
 
-				<p>Our client is one of the largest investment houses in the UK and one of Europe’s leading asset managers with over £240 billion in funds under management. Their company culture is famous. They have a strong belief in their staff having a great work-life balance. They have a very friendly working atmosphere and high staff morale. This emphasis on the culture of the workplace makes this global asset manager one of the best places to work, with unusually low staff turnover. This company look at the internship as an 8 week interview for the graduate scheme. The graduate scheme offers incredible training, a high salary, professional qualifications including the IMC and diverse rotations across the business.</p>
+				<p>It is a UK charity offering graduates the opportunity to teach, inspire and change the lives of students in challenging circumstances. Their goal is to create future leaders who can raise pupil’s achievement levels and help break the link between low family income and poor educational attainment. Teach First are sponsored by many Times Top 100 Graduate Employers including Accenture, McKinsey, Deloitte, Goldman Sachs, UBS, Credit Suisse and Google, who actively recruit graduates who have completed the Teach First scheme.</p>
 			
-				<h2>The Internships</h2>
-				
-				<h3>Information Systems</h3>
-				<p>Information Systems is a support function within the company that delivers innovative solutions to solve their IT problems. The company’s focus on creating and testing their own software makes them unique in the finance industry and a very exciting place to work in for any person interested in IT.  The internship will be in one of the following areas: Architecture, Development, Testing & Release & Support.</p>
-				<p>This 8 week summer internship is for anyone studying any degree but who likes technology and has demonstrable interest in pursuing a career in technology. You don’t have to be technically proficient (but there are projects for those that are).</p>
-				
-				<h3>Change Management</h3>
-				<p>Change Management.  Business and Technology Change is the primary interface for all of the company’s business units, working closely with each to understand the requirements for change and ultimately deliver the defined objectives within a project life-cycle environment. Projects can range from improvements to existing business solutions, implementation of new business and IT solutions, through to multi-million pound revenue generating business initiatives.</p>
-				<p>This 8 week summer internship is for anyone studying any degree who is interested in business strategy, technology and stakeholder management. You don’t need to be technically proficient.</p>
-				
-				<h3>Year Long Placement - Information Systems</h3>
-				<p>Year Long Placement - Information Systems: As the Information Systems internship above, but 12 months in duration with rotations around Architecture, Development, Testing & Release & Support. </p>
+				<h2>What is the Leadership Development Scheme?</h2>
+<p>After intensive training, the prestigious 2-year scheme is spent teaching in a disadvantaged school where you take on real responsibility from day one. You will train for a fully funded PGCE and become a ‘newly qualified teacher’ in your second year, enjoying a salary increase of £4k. The scheme also lets you complete a summer internship with one of their sponsor companies which frequently leads to a graduate role at the end of the scheme.</p>
+
+				<h2>Interested?</h2>
+<p>If you graduate in 2015 and would like to hear more about the scheme from Teach First themselves then please upload your details here <a href="http://bit.ly/1x1uqD3">http://bit.ly/1x1uqD3</a> quoting ‘DUR NATSCI’ as the referral code.</p>
 			</div>
 			<div class="sidebar">
 				<?php
